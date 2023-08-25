13a7c3316337e74f9f3160964a4202486c889217
Konrad Lorenz <konrad-lorenz@outlook.com>
2023-08-25 12:16:45 +0200

    Routing fix

---
+++ nextjs-blog/pages/_app.js
@@ -0,0 +1,8 @@
+import Layout from "../Components/layout";
+
+export default function MyApp({ Component, pageProps }) {
+    return(
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    ) }
