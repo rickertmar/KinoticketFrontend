3c2ca367f8fd8dc7fdcdb68ceccbe55537b9cfc2
Uiyoung Kim <uiyoung.kim@evorait.com>
2023-08-26 01:14:36 +0200

    minor changes

--- nextjs-blog/pages/Contact.js
+++ nextjs-blog/pages/Contact.js
@@ -4,9 +4,10 @@ import styles from '../styles/Home.module.css';
 function Contact() {
   return (
     <div>
+      <MyImage />
         <h1>Hier Kommt Conntent FFFFFF</h1>
       {/* ... other components or content ... */}
-      <MyImage />
+      
       {/* ... other components or content ... */}
     </div>
   );
