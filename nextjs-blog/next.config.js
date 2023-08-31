aea5ae23a5c00c9a89dc914eb658521037e5c7ec
Konrad Lorenz <86967062+K-Lorenz@users.noreply.github.com>
2023-08-31 12:52:07 +0200

    Connection to backend

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
     output: "standalone",
+        async rewrites() {
+          return [
+            {
+              source: '/api/:path*',
+              destination: 'http://kinoticket.azurewebsites.net/:path*',
+            },
+          ];
+        },
+      
 }
--- nextjs-blog/pages/information.js
+++ nextjs-blog/pages/information.js
@@ -1,6 +1,28 @@
 import styles from '../styles/Home.module.css';
 
 function Infos() {
+  const onTestButtonClick = async (e) => {
+    e.preventDefault();
+    console.log('Bearer '+ localStorage.getItem('access_token'))
+    // Make API request to authenticate
+    try {
+      const response = await fetch('api/users', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer '+ localStorage.getItem('access_token')
+        },
+      });
+
+      if (response.ok) {
+        console.log(response);
+      } else {
+        // Handle authentication failure
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
   return (
     <div>
       <h1 className={styles.title}>Info page</h1>
@@ -15,6 +37,7 @@ function Infos() {
         <p>Our location in Baden-Württemberg features two multiplex cinemas right in the heart of the city. With 18 theaters and approximately 3,600 seats, we offer the city an enormous variety and serve as Mannheim's cultural hub. Nearly one million visitors come here annually for top-notch cinema experiences. Perfect sound and supreme seating comfort with modern D-Box seats offer the highest level of cinematic enjoyment.</p>
         <p>&nbsp;</p>
         <p>Thanks to our two locations, we can provide a wide range of entertainment options. From big blockbusters to thought-provoking art-house films, as well as films in their original languages, there's something for every taste. Live events such as broadcasts from the Metropolitan Opera and many other global cities are, of course, also available.</p>
+        <button onClick={onTestButtonClick}>Test Connection</button>
       </div>
 
       <div className={styles.imageContainer}>
