a7ca117fb302a00f3c46479c0701561edacb4ba4
Konrad Lorenz <86967062+K-Lorenz@users.noreply.github.com>
2023-09-04 15:47:26 +0200

    Revert " new Contact page"

--- nextjs-blog/components/footer.js
+++ nextjs-blog/components/footer.js
@@ -1,12 +1,8 @@
 function Footer() {
     return (
       <footer>
-        <div className="mx-auto max-w-2xl text-center py-8">
-        <h3 className="text-xl font-medium" style={{ color: "#90DDF0" }}>Contact Us</h3>
-        <p style={{ color: "#ebebeb" }}>Our service team is happy to assist you:</p>
-        <p style={{ color: "#ebebeb" }}>By phone at 0621 / xx xx xx (14 ct/min, max. 42 ct/min), daily from 2:00 PM to 8:00 PM</p>
-        <p style={{ color: "#ebebeb" }}>Via email at service@dhbwkino.de</p>
-      </div>
+        <p></p>
+        {/* oder wozu brauchen wir eig Footer */}
       </footer>
     );
   }
