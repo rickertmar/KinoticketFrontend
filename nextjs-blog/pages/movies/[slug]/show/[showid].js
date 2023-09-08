5d2ef0598a1ba3c778dc70bff8dbf450a3a88d18
Uiyoung Kim <s221683@student.dhbw-mannheim.de>
2023-09-09 01:29:20 +0200

    design optimation

--- nextjs-blog/pages/movies/[slug]/show/[showid].js
+++ nextjs-blog/pages/movies/[slug]/show/[showid].js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
-function Seat({ id, number, row, isAvailable, selected, onClick }) {
+function Seat({ id, number, isAvailable, selected, onClick }) {
+  const seatStyles = {
+    cursor: isAvailable ? 'pointer' : 'not-allowed',
+    backgroundColor: isAvailable
+      ? selected
+        ? 'red'
+        : 'green'
+      : 'gray',
+    color: 'white',
+    borderRadius: '8px',
+    padding: '8px',
+    textAlign: 'center',
+    width: '40px',
+    height: '40px',
+  };
+
   return (
     <div
       onClick={() => {
@@ -8,13 +23,7 @@ function Seat({ id, number, row, isAvailable, selected, onClick }) {
           onClick(id);
         }
       }}
-      className={`p-3 border ${
-        isAvailable
-          ? selected
-            ? 'bg-red-500 text-white cursor-not-allowed'
-            : 'bg-green-500 text-white cursor-pointer hover:bg-green-600'
-          : 'bg-gray-300 text-gray-600 cursor-not-allowed'
-      } rounded-lg text-center`}
+      style={seatStyles}
     >
       {number}
     </div>
@@ -27,7 +36,7 @@ export default function SeatSelection() {
 
   useEffect(() => {
     // Fetch the seat data from the JSON file
-    fetch('/seatsData.json') 
+    fetch('/seatsData.json')
       .then((response) => response.json())
       .then((data) => setSeatsData(data))
       .catch((error) => console.error('Error fetching seat data:', error));
@@ -43,16 +52,21 @@ export default function SeatSelection() {
   };
 
   return (
-    <div className="bg-gray-200 min-h-screen flex justify-center items-center">
+    <div className="bg-gray-200 min-h-screen flex flex-col items-center">
+      <h2 className="text-2xl font-semibold mb-4 mt-4">Select Your Seats</h2>
+      <div className="flex justify-center items-center w-full h-20 mb-4">
+        <div
+          className="w-3/5 h-1 bg-black"
+          style={{ backgroundColor: 'black' }} // You can adjust the color here
+        ></div>
+      </div>
       <div className="bg-white p-8 rounded-lg shadow-md">
-        <h2 className="text-2xl font-semibold mb-4">Select Your Seats</h2>
-        <div className="grid grid-cols-5 gap-4">
+        <div className="grid grid-cols-8 gap-4"> {/* Adjust the number of columns here */}
           {seatsData.map((seat) => (
             <Seat
               key={seat.id}
               id={seat.id}
               number={seat.number}
-              row={seat.row}
               isAvailable={seat.available}
               selected={selectedSeats.includes(seat.id)}
               onClick={toggleSeat}
