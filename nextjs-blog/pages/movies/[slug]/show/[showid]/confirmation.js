cfea492bc5df6fc3ffe71acae1a304f67b00f892
zxTyphoon <seantylerstraub@gmail.com>
2023-09-17 11:29:40 +0200

    "Added price and Tailwind colors"

--- nextjs-blog/pages/movies/[slug]/show/[showid]/confirmation.js
+++ nextjs-blog/pages/movies/[slug]/show/[showid]/confirmation.js
@@ -5,8 +5,33 @@ import React, { useEffect, useState } from 'react';
 export default function ConfirmationPage() {
   const router = useRouter();
   const { selectedSeats, ticketTypes, showid, slug } = router.query;
-  const [movie, setMovie] = useState(null);
-  const [showTime, setShowTime] = useState('N/A');
+  const ticketTypeToNumericPrice = (type) => {
+    switch (type) {
+      case 'Student':
+        return 8;
+      case 'Child':
+        return 6;
+      case 'Regular':
+        return 10;
+      default:
+        return 0;
+    }
+  };
+  const calculateTotalPrice = () => {
+    return Object.values(parsedTicketTypes).reduce((sum, type) => sum + ticketTypeToNumericPrice(type), 0);
+  };
+  const ticketTypeToPrice = (type) => {
+    switch (type) {
+      case 'Student':
+        return '8€';
+      case 'Child':
+        return '6€';
+      case 'Regular':
+        return '10€';
+      default:
+        return 'Unknown Price';
+    }
+  };
   const formatSlugToTitle = (slug) => {
     return slug
       .split('-')
@@ -52,44 +77,47 @@ export default function ConfirmationPage() {
       <Head>
         <title>Payment Page</title>
       </Head>
-      <main style={{ backgroundColor: '#EBEBEB' }} className="mt-5">
-        <div className="flex flex-row flex-nowrap max-w-7xl mx-auto p-6">
+      <main className="bg-primary-20 mt-5 text-white">
+        <div className="flex flex-row flex-nowrap max-w-7xl mx-auto p-6 text-white">
           <div className="flex flex-col w-full">
-            <h2 style={{ color: '#2D3339' }} className="text-3xl font-semibold mb-4 text-center w-full">
+            <h2 className="text-3xl font-semibold mb-4 text-center w-full">
               Payment for Movie: {slug ? formatSlugToTitle(slug) : 'Unknown Movie'}
               <div>
-                Showtime: {showTime}
+                Showtime: N/A
               </div>
             </h2>
             <div className="mb-4 w-full">
-              <h3 style={{ color: '#2D3339' }} className="text-xl font-bold mb-2">Selected Seats and Types</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Selected Seats and Types
+              </h3>
               <ul>
                 {parsedSeats.map((seatId, index) => (
                   <li key={index}>
-                    {seatIdToInfo[seatId] ?
-                      `Seat ${seatIdToInfo[seatId].seatRow}${seatIdToInfo[seatId].number}: ${parsedTicketTypes[seatId] || 'Unknown Type'}` :
-                      `Seat ID ${seatId}: ${parsedTicketTypes[seatId] || 'Unknown Type'}`
+                    {seatIdToInfo[seatId]
+                      ? `Seat ${seatIdToInfo[seatId].seatRow}${seatIdToInfo[seatId].number}: ${parsedTicketTypes[seatId] || 'Unknown Type'} - ${ticketTypeToPrice(parsedTicketTypes[seatId])}`
+                      : `Seat ID ${seatId}: ${parsedTicketTypes[seatId] || 'Unknown Type'} - ${ticketTypeToPrice(parsedTicketTypes[seatId])}`
                     }
                   </li>
                 ))}
               </ul>
+              <br />
+              <div>
+                Total Price: {calculateTotalPrice()}€
+              </div>
             </div>
-
-            {/* Payment form */}
+  
             <div className="mb-4 w-full flex justify-between flex-wrap">
               <form onSubmit={handlePaymentSubmit} className="w-full">
                 <button
                   type="submit"
-                  style={{ backgroundColor: '#90DDF0', color: '#2D3339' }}
-                  className="transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg mb-4"
+                  className="transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg mb-4 bg-accent-40"
                 >
                   Submit Payment
                 </button>
               </form>
               <button
                 onClick={handleCancel}
-                style={{ backgroundColor: '#FF8383', color: '#2D3339' }}
-                className="transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg mb-4"
+                className="transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg mb-4 bg-red-400"
               >
                 Cancel
               </button>
@@ -99,4 +127,4 @@ export default function ConfirmationPage() {
       </main>
     </>
   );
-}
+}  
--- nextjs-blog/pages/movies/[slug]/show/[showid]/ticketselection.js
+++ nextjs-blog/pages/movies/[slug]/show/[showid]/ticketselection.js
@@ -1,21 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Head from 'next/head';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function TicketSelection() {
   const [ticketTypes, setTicketTypes] = useState({});
   const [seatIdToInfo, setSeatIdToInfo] = useState({});
   const router = useRouter();
   const { selectedSeats, showid, slug } = router.query;
-  const parsedSeats = JSON.parse(selectedSeats || '[]');
+  const parsedSeats = JSON.parse(selectedSeats || "[]");
 
   useEffect(() => {
-    fetch('/seatsData.json')
+    fetch("/seatsData.json")
       .then((response) => response.json())
       .then((seatsData) => {
         const newSeatIdToInfo = {};
         seatsData.forEach((seat) => {
-          newSeatIdToInfo[seat.id] = { seatRow: seat.seatRow, number: seat.number };
+          newSeatIdToInfo[seat.id] = {
+            seatRow: seat.seatRow,
+            number: seat.number,
+          };
         });
         setSeatIdToInfo(newSeatIdToInfo);
       });
@@ -26,11 +29,15 @@ export default function TicketSelection() {
   };
 
   const handlePayment = () => {
-    const selectedSeatsList = Object.keys(ticketTypes).map((seatId) => {
-      const seatInfo = seatIdToInfo[seatId];
-      const seatStr = seatInfo ? `${seatInfo.seatRow}${seatInfo.number}` : `ID ${seatId}`;
-      return `Seat ${seatStr} (${ticketTypes[seatId]})`;
-    }).join(', ');
+    const selectedSeatsList = Object.keys(ticketTypes)
+      .map((seatId) => {
+        const seatInfo = seatIdToInfo[seatId];
+        const seatStr = seatInfo
+          ? `${seatInfo.seatRow}${seatInfo.number}`
+          : `ID ${seatId}`;
+        return `Seat ${seatStr} (${ticketTypes[seatId]})`;
+      })
+      .join(", ");
 
     router.push({
       pathname: `/movies/${router.query.slug}/show/${router.query.showid}/confirmation`,
@@ -38,13 +45,10 @@ export default function TicketSelection() {
         selectedSeats: JSON.stringify(parsedSeats),
         ticketTypes: JSON.stringify(ticketTypes),
         showid: showid,
-        slug: slug
-      }
+        slug: slug,
+      },
     });
   };
-  
-
-  
 
   const setTicketTypeForSeat = (seat, type) => {
     setTicketTypes({
@@ -58,38 +62,41 @@ export default function TicketSelection() {
       <Head>
         <title>Select your ticket type</title>
       </Head>
-      <main style={{ backgroundColor: '#EBEBEB' }} className="mt-5">
+      <main className="bg-primary-20 mt-5 text-white">
         <div className="flex flex-row flex-nowrap max-w-7xl mx-auto p-6">
           <div className="flex flex-col w-full">
-            <h2 style={{ color: '#2D3339' }} className="text-3xl font-semibold mb-4 text-center w-full">Select your ticket type</h2>
+            <h2 className="text-3xl font-semibold mb-4 text-center w-full">
+              Select your ticket type
+            </h2>
   
             {parsedSeats.map((seatId, index) => (
               <div key={index} className="mb-4 w-full">
-                <label style={{ color: '#2D3339' }} className="block font-bold mb-2">
-                  {seatIdToInfo[seatId] ? 
-                    `Ticket Type for Seat ${seatIdToInfo[seatId].seatRow}${seatIdToInfo[seatId].number}` : 
-                    `Ticket Type for Seat ID ${seatId}`
-                  }
+                <label className="block font-bold mb-2">
+                  {seatIdToInfo[seatId]
+                    ? `Ticket Type for Seat ${seatIdToInfo[seatId].seatRow}${seatIdToInfo[seatId].number}`
+                    : `Ticket Type for Seat ID ${seatId}`}
                 </label>
                 <select
-                  style={{ borderColor: '#90DDF0' }}
-                  className="w-full p-3 rounded-lg focus:outline-none"
-                  value={ticketTypes[seatId] || ''}
+                  className="border border-custom-blue w-full p-3 rounded-lg focus:outline-none text-gray-900"
+                  value={ticketTypes[seatId] || ""}
                   onChange={(e) => setTicketTypeForSeat(seatId, e.target.value)}
                 >
-                  <option value="" disabled>Select ticket type</option>
-                  <option value="Student">Student</option>
-                  <option value="Child">Child</option>
-                  <option value="Regular">Regular</option>
+                  <option value="" disabled>
+                    Select ticket type
+                  </option>
+                  <option value="Student">Student - 8€</option>
+                  <option value="Child">Child - 6€</option>
+                  <option value="Regular">Regular - 10€</option>
                 </select>
               </div>
             ))}
-            
+  
             <div className="text-center w-full">
               <button
-                style={{ backgroundColor: '#90DDF0', color: '#2D3339' }}
-                className={`transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg ${
-                  parsedSeats.some((seatId) => !ticketTypes[seatId]) ? 'opacity-50 cursor-not-allowed' : ''
+                className={`transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg bg-accent-40 ${
+                  parsedSeats.some((seatId) => !ticketTypes[seatId])
+                    ? "opacity-50 cursor-not-allowed"
+                    : ""
                 }`}
                 disabled={parsedSeats.some((seatId) => !ticketTypes[seatId])}
                 onClick={handlePayment}
@@ -97,12 +104,11 @@ export default function TicketSelection() {
                 Proceed to Payment
               </button>
             </div>
-
-            <div className="text-center w-full">
+  
+            <div className="text-center w-full mt-4">
               <button
                 onClick={handleCancel}
-                style={{ backgroundColor: '#FF8383', color: '#2D3339' }}
-                className="transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg mt-4"
+                className="transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg bg-red-400"
               >
                 Cancel
               </button>
@@ -112,5 +118,4 @@ export default function TicketSelection() {
       </main>
     </>
   );
-  
 }  
