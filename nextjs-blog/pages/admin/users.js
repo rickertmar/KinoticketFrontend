b686948b0354ef3d682bf6afdca4663b9f59acee
Uiyoung Kim <s221683@student.dhbw-mannheim.de>
2023-10-01 23:11:52 +0200

    Admin Dashboard basic structure

---
+++ nextjs-blog/pages/admin/users.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const users = [
+  { id: 1, name: 'John Doe', role: 'Admin', status: 'Active' },
+  { id: 2, name: 'Jane Smith', role: 'User', status: 'Inactive' },
+  // Add more users as needed
+];
+
+const UserManagement = () => {
+  return (
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      {/* ... (sidebar code remains the same) */}
+
+      {/* Main Content */}
+      <main className="flex-1 p-4 bg-gray-100">
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-2xl font-semibold mb-4">User Management</h1>
+          <div className="bg-white p-4 rounded-lg shadow-md w-full overflow-x-auto">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead>
+                <tr>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
+                  <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.id}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.name}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.role}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{user.status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default UserManagement;
