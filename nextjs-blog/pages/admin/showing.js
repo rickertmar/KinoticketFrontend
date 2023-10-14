f337bebc8e1619c8b57beaa4041bd3f2bf5fddbf
Uiyoung Kim <s221683@student.dhbw-mannheim.de>
2023-10-14 23:27:25 +0200

    Admin-Dashboard new Showing page Design

--- nextjs-blog/pages/admin/showing.js
+++ nextjs-blog/pages/admin/showing.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Head from 'next/head';
 
 const Showing = () => {
   const [showings, setShowings] = useState([]);
@@ -49,112 +50,219 @@ const Showing = () => {
     setFormData({ ...formData, [name]: value });
   };
 
+ 
+
+  const handleDelete = async (showingId) => {
+    try {
+      // Make a DELETE request to delete the showing with the given ID
+      await axios.delete(`${process.env.API_URL}/showings/${showingId}`);
+      // After successful deletion, fetch updated showings and remove the deleted item from the state
+      fetchShowings();
+      // Remove the deleted showing from the state
+      setShowings((prevShowings) => prevShowings.filter((showing) => showing.id !== showingId));
+    } catch (error) {
+      console.error('Error deleting showing:', error);
+    }
+  };
+  
+
+  // Sample test data for existing showings
+  const sampleShowings = [
+    {
+      id: 1,
+      time: '2023-09-15T14:30',
+      showingExtras: '3D',
+      movieId: 101,
+      cinemaHallId: 1,
+      seatPrice: 12.99,
+    },
+    {
+      id: 2,
+      time: '2023-09-15T17:00',
+      showingExtras: '2D',
+      movieId: 102,
+      cinemaHallId: 2,
+      seatPrice: 10.99,
+    },
+  ];
   return (
-    <div className="min-h-screen bg-primary-50 p-8">
-     
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-semibold mb-4">Manage Showings</h1>
-        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg p-6 mb-8">
-          {/* Form inputs for creating a new showing */}
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+    <div className="flex justify-center items-center">
+      <Head>
+        <title>Manage Showings - DHBW Kino</title>
+        <meta
+          name="description"
+          content="Create and manage showings for DHBW Kino. Set showing time, extras, movie, cinema hall, and seat price."
+        />
+      </Head>
+      <div className="mt-10 px-20 py-5">
+        <div className="mt-2 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+          <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl text-accent-50">
+            Manage Showings
+          </h2>
+        </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
-              <label className="block text-gray-700 text-sm font-bold mb-2">Showing Time</label>
+              <label htmlFor="time" className="block text-sm font-medium text-white">
+                Showing Time
+              </label>
               <input
                 type="datetime-local"
                 name="time"
+                id="time"
+                className="mt-1 p-3 w-full rounded-md border border-gray-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
                 value={formData.time}
                 onChange={handleInputChange}
-                className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 required
               />
             </div>
             <div>
-  <label className="block text-gray-700 text-sm font-bold mb-2">Showing Extras</label>
-  <select
-    name="showingExtras"
-    value={formData.showingExtras}
-    onChange={handleInputChange}
-    className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-  >
-    <option value="">Select an option</option>
-    <option value="2D">2D</option>
-    <option value="3D">3D</option>
-    <option value="4D">4D</option>
-  </select>
-</div>
-          </div>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+              <label htmlFor="showing-extras" className="block text-sm font-medium text-white">
+                Showing Extras
+              </label>
+              <select
+                name="showingExtras"
+                id="showing-extras"
+                className="mt-1 p-3 w-full rounded-md border border-gray-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
+                value={formData.showingExtras}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="">Select an option</option>
+                <option value="2D">2D</option>
+                <option value="3D">3D</option>
+                <option value="4D">4D</option>
+              </select>
+            </div>
             <div>
-              <label className="block text-gray-700 text-sm font-bold mb-2">Movie ID</label>
+              <label htmlFor="movie-id" className="block text-sm font-medium text-white">
+                Movie ID
+              </label>
               <input
                 type="number"
                 name="movieId"
+                id="movie-id"
+                className="mt-1 p-3 w-full rounded-md border border-gray-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
                 value={formData.movieId}
                 onChange={handleInputChange}
-                className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 required
               />
             </div>
             <div>
-              <label className="block text-gray-700 text-sm font-bold mb-2">Cinema Hall ID</label>
+              <label htmlFor="cinema-hall-id" className="block text-sm font-medium text-white">
+                Cinema Hall ID
+              </label>
               <input
                 type="number"
                 name="cinemaHallId"
+                id="cinema-hall-id"
+                className="mt-1 p-3 w-full rounded-md border border-gray-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
                 value={formData.cinemaHallId}
                 onChange={handleInputChange}
-                className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 required
               />
             </div>
-          </div>
-          <div>
-            <label className="block text-gray-700 text-sm font-bold mb-2">Seat Price</label>
-            <input
-              type="number"
-              name="seatPrice"
-              value={formData.seatPrice}
-              onChange={handleInputChange}
-              className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="mt-4 bg-accent-50 hover:bg-accent-40 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Create Showing
-          </button>
-        </form>
+            <div>
+              <label htmlFor="seat-price" className="block text-sm font-medium text-white">
+                Seat Price
+              </label>
+              <input
+                type="number"
+                name="seatPrice"
+                id="seat-price"
+                className="mt-1 p-3 w-full rounded-md border border-gray-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black"
+                value={formData.seatPrice}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-3 px-4 bg-accent-30 text-white font-medium rounded-md hover:bg-accent-40 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            >
+              Create Showing
+            </button>
+          </form>
+        </div>
 
-        {/* Display existing showings */}
-        <div>
-  <h2 className="text-2xl font-semibold mb-4">Existing Showings</h2>
+        // Display existing showings
+<div className="mt-10">
+  <h2 className="flex justify-center items-center text-2xl font-semibold text-accent-50 mb-4">Existing Showings</h2>
+  
   <ul>
-    {showings.map((showing) => (
-      <li key={showing.id} className="bg-white shadow-md rounded-lg p-4 mb-4">
-        <p>
-          <strong className="text-gray-700">Showing Time:</strong> {showing.time}
-        </p>
-        <p>
-          <strong className="text-gray-700">Showing Extras:</strong> {showing.showingExtras || 'N/A'}
-        </p>
-        <p>
-          <strong className="text-gray-700">Movie ID:</strong> {showing.movieId}
-        </p>
-        <p>
-          <strong className="text-gray-700">Cinema Hall ID:</strong> {showing.cinemaHallId}
-        </p>
-        <p>
-          <strong className="text-gray-700">Seat Price:</strong> {showing.seatPrice}
-        </p>
-      </li>
-    ))}
+    {showings.length > 0 ? (
+      showings.map((showing) => (
+        <li key={showing.id} className="bg-white shadow-md rounded-lg p-4 mb-4">
+          <p>
+            <strong className="text-gray-700">Showing Time:</strong> {showing.time}
+          </p>
+          <p>
+            <strong className="text-gray-700">Showing Extras:</strong>{' '}
+            {showing.showingExtras || 'N/A'}
+          </p>
+          <p>
+            <strong className="text-gray-700">Movie ID:</strong> {showing.movieId}
+          </p>
+          <p>
+            <strong className="text-gray-700">Cinema Hall ID:</strong> {showing.cinemaHallId}
+          </p>
+          <p>
+            <strong className="text-gray-700">Seat Price:</strong> ${showing.seatPrice.toFixed(2)}
+          </p>
+          <div className="mt-2">
+            <button
+              onClick={() => handleEdit(showing)}
+              className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => handleDelete(showing.id)}
+              className="bg-red-500 text-white px-4 py-2 rounded-md"
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      ))
+    ) : sampleShowings.length > 0 ? (
+      sampleShowings.map((showing) => (
+        <li key={showing.id} className="bg-white shadow-md rounded-lg p-4 mb-4">
+          <p>
+            <strong className="text-gray-700">Showing Time:</strong> {showing.time}
+          </p>
+          <p>
+            <strong className="text-gray-700">Showing Extras:</strong> {showing.showingExtras || 'N/A'}
+          </p>
+          <p>
+            <strong className="text-gray-700">Movie ID:</strong> {showing.movieId}
+          </p>
+          <p>
+            <strong className="text-gray-700">Cinema Hall ID:</strong> {showing.cinemaHallId}
+          </p>
+          <p>
+            <strong className="text-gray-700">Seat Price:</strong> ${showing.seatPrice.toFixed(2)}
+          </p>
+          <div className="flex justify-center items-center mt-2 ">
+         
+            <button
+              onClick={() => handleDelete(showing.id)}
+              className="bg-red-500 text-white px-4 py-2 rounded-md"
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      ))
+    ) : (
+      <p className="text-white">No showings available.</p>
+    )}
   </ul>
 </div>
+
 
       </div>
     </div>
   );
 };
-
 export default Showing;
