9fa154303dfc5b8dfab6a7d176d38740c54ad01d
Konrad Lorenz <konrad-lorenz@outlook.com>
2023-10-26 23:51:31 +0200

    fix for sidebar

--- nextjs-blog/components/leftSidebar.js
+++ nextjs-blog/components/leftSidebar.js
@@ -1,17 +1,17 @@
 export default function LeftSidebar(){
     return(
         <div className="big:flex flex-col gap-10 mt-28 mr-10 hidden">
-            <div className="h-fit w-48">
+            <div className="h-fit w-48 p-2 bg-neutral-900">
                 <p className="text-white">This could be a space for Ads or Movie Previews</p>
                 <img src="https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"></img>
                 <p className="text-white">Some text below</p>
             </div>
-            <div className="h-fit w-48">
+            <div className="h-fit w-48 p-2 bg-neutral-900">
                 <p className="text-white">This could be a space for Ads or Movie Previews</p>
                 <img src="https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"></img>
                 <p className="text-white">Some text below</p>
             </div>
-            <div className="h-fit w-48">
+            <div className="h-fit w-48 p-2 bg-neutral-900">
                 <p className="text-white">This could be a space for Ads or Movie Previews</p>
                 <img src="https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"></img>
                 <p className="text-white">Some text below</p>
