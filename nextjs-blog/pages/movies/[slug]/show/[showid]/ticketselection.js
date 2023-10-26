a6e24573b4f2d5a150cdbc4052dd755837930b27
zxTyphoon <seantylerstraub@gmail.com>
2023-10-26 02:02:01 +0200

    "I CANT GET IT TO DO WHAT I WANT, minor changes"

--- nextjs-blog/pages/movies/[slug]/show/[showid]/ticketselection.js
+++ nextjs-blog/pages/movies/[slug]/show/[showid]/ticketselection.js
@@ -2,37 +2,26 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 
-export default function TicketSelection() {
+export default function TicketSelection({ isAuthenticated }) {
   const router = useRouter();
   const { selectedSeats, showid, slug } = router.query;
   const parsedSeats = JSON.parse(selectedSeats || "[]");
 
-  const [ticketTypes, setTicketTypes] = useState({ Regular: parsedSeats.length, Student: 0, Child: 0 });
+const [ticketTypes, setTicketTypes] = useState({ Regular: parsedSeats.length, Student: 0, Child: 0 });
   const [seatIdToInfo, setSeatIdToInfo] = useState({});
-
-  useEffect(() => {
-    fetch("/seatsData.json")
-      .then((response) => response.json())
-      .then((seatsData) => {
-        const newSeatIdToInfo = {};
-        seatsData.forEach((seat) => {
-          newSeatIdToInfo[seat.id] = {
-            seatRow: seat.seatRow,
-            number: seat.number,
-          };
-        });
-        setSeatIdToInfo(newSeatIdToInfo);
-      });
-  }, []);
 
   const handleCancel = () => {
     router.back();
   };
 
   const handlePayment = () => {
+    if (!isAuthenticated) {
+      
+      alert("You must be logged in to proceed to payment.");
+      return;}
     const totalSeats = parsedSeats.length;
     const totalPrice = ticketTypes.Regular * 10 + ticketTypes.Student * 8 + ticketTypes.Child * 6;
-  
+
     router.push({
       pathname: `/movies/${router.query.slug}/show/${router.query.showid}/confirmation`,
       query: {
@@ -45,12 +34,12 @@ export default function TicketSelection() {
       },
     });
   };
-  
+
 
   const adjustTicketTypeCount = (type, delta) => {
     const newCount = Math.max(0, ticketTypes[type] + delta);
     let totalTickets = Object.values(ticketTypes).reduce((a, b) => a + b, 0) - ticketTypes[type] + newCount;
-  
+
     if (totalTickets <= parsedSeats.length) {
       setTicketTypes({
         ...ticketTypes,
@@ -77,7 +66,7 @@ export default function TicketSelection() {
       }
     }
   };
-  
+
 
   return (
     <>
@@ -92,27 +81,27 @@ export default function TicketSelection() {
             </h2>
 
             {[
-  { label: "Regular 10€", type: "Regular" },
-  { label: "Student 8€", type: "Student" },
-  { label: "Child 6€", type: "Child" },
-].map(({ label, type }) => (
-  <div key={type} className="mb-4 w-full">
-    <label className="block font-bold mb-2">
+              { label: "Regular 10€", type: "Regular" },
+              { label: "Student 8€", type: "Student" },
+              { label: "Child 6€", type: "Child" },
+            ].map(({ label, type }) => (
+              <div key={type} className="mb-4 w-full">
+                <label className="block font-bold mb-2">
       {label}
     </label>
-    <button onClick={() => adjustTicketTypeCount(type, -1)} className="bg-accent-40 text-white px-2 py-1 rounded-l">-</button>
-    <span className="px-4">{ticketTypes[type]}</span>
-    <button onClick={() => adjustTicketTypeCount(type, 1)} className="bg-accent-40 text-white px-2 py-1 rounded-r">+</button>
-  </div>
-))}
+                <button onClick={() => adjustTicketTypeCount(type, -1)} className="bg-accent-40 text-white px-2 py-1 rounded-l">-</button>
+                <span className="px-4">{ticketTypes[type]}</span>
+                <button onClick={() => adjustTicketTypeCount(type, 1)} className="bg-accent-40 text-white px-2 py-1 rounded-r">+</button>
+              </div>
+            ))}
 
             <div className="text-center w-full">
-              <button
-                onClick={handlePayment}
-                className="transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg bg-accent-40"
-              >
-                Proceed to Payment
-              </button>
+                <button
+                  onClick={handlePayment}
+                  className="transition duration-300 ease-in-out font-bold py-3 px-6 rounded-lg bg-accent-40"
+                >
+                  Proceed to Payment
+                </button>
             </div>
 
             <div className="text-center w-full mt-4">
